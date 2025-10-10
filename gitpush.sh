96b8fa089d73ac49b7b37db147d807094a6d0037
Jonathan Costa <jhoncosta2@gmail.com>
2025-10-10 06:56:37 -0300

    teste2

--- gitpush.sh
+++ gitpush.sh
@@ -1,16 +1,18 @@
 #! /bin/bash/expect -f
 
 # load .env
-source .env
+#source .env
 
 read -e -p "Type the commit message: " message
 git add .
 git commit -m"$message"
 
-spawn git push
+#spawn git push
 
-expect "Enter passphrase for key '/home/jhon/.ssh/id_ed25519'"
+git push
 
-send "$GITPASS\r"
+#expect "Enter passphrase for key '/home/jhon/.ssh/id_ed25519'"
 
-expect eof
+#send "$GITPASS\r"
+
+#expect eof
